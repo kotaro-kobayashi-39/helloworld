75bff8b9bc08e56d9478f40b3aac46eabadb78b1
03TA <wanpaku1722@icloud.com>
2023-07-20 11:56:25 +0900

    Merge branch 'Beta' of https://github.com/khachiyathu/helloworld into Beta

--- member.py
+++ member.py
@@ -1,4 +1,4 @@
-ver = "0.8"
+ver = "0.9"
 
 
 class Member:
@@ -22,23 +22,36 @@ newmember = Member("笹谷拓斗", "よろしく")
 mlist.append(newmember)
 
 newmember = Member("蜂屋 孝太郎", "アタオカです！")
+newmember = Member("岡 拓未", "よろしくです！")
 mlist.append(newmember)
-
-newmember = Member("房州優樹", "よろしくです！")
-mlist.append(newmember)
-
 newmember = Member("吉田 羅生", "よろしくです！")
 mlist.append(newmember)
-
 newmember = Member("大河原翔太", "よろしくお願いいたします。")
 mlist.append(newmember)
-
+newmember = Member("房州優樹", "よろしくです！")
+mlist.append(newmember)
+newmember = Member("吉田 羅生", "よろしくです！")
+mlist.append(newmember)
+newmember = Member("蜂屋 孝太郎", "アタオカです！")
+mlist.append(newmember)
+newmember = Member("房州優樹", "よろしくです！")
+mlist.append(newmember)
+newmember = Member("吉田 羅生", "よろしくです！")
+mlist.append(newmember)
+newmember = Member("大河原翔太", "よろしくお願いいたします。")
+mlist.append(newmember)
+newmember = Member("渡邉 雄太", "よろしく！")
+mlist.append(newmember)
+newmember = Member("房州優樹", "よろしくです！")
+mlist.append(newmember)
+newmember = Member("吉田 羅生", "よろしくです！")
+mlist.append(newmember)
 newmember = Member("渡邉　雄太", "よろしく！")
 mlist.append(newmember)
-
 newmember = Member("岩崎泰斗", "おなかへりました")
 mlist.append(newmember)
-
+newmember = Member("岸野航", "よろしくです！")
+mlist.append(newmember)
 
 # メンバー表示
 print("各メンバーから一言")
